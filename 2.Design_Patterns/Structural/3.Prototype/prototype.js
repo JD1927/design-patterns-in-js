6cd3567769bb88b326f1e14ba098b25ce1bc085d
Juan Aguirre <j1927aguirre@gmail.com>
2024-09-17 20:33:38 -0500

    feat: add prototype coding exercise and summary

--- 2.Design_Patterns/Structural/3.Prototype/prototype.js
+++ 2.Design_Patterns/Structural/3.Prototype/prototype.js
@@ -10,7 +10,10 @@
   Prototype: A partially or fully initialized object that you copy (clone) and make use of
 
   Summary:
-  -
+  - To implement a prototype, partially construct an object and store it somewhere
+  - Deep copy the prototype
+  - Customize the resulting instance
+  - A factory provides a convenient API for using prototypes
 
  */
 
