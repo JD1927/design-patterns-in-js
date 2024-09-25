af51cd2e6ba078d0d6108e7d418067af2e318dc1
Juan Aguirre <j1927aguirre@gmail.com>
2024-09-24 21:04:08 -0500

    feat: add adapter pattern coding exercise

--- 2.Design_Patterns/Structural/5.Adapter/adapter.js
+++ 2.Design_Patterns/Structural/5.Adapter/adapter.js
@@ -7,7 +7,10 @@
   Adapter: A construct which adapts an existing interface X to conform to the required interface Y
 
   Summary:
-  - 
+  - Implementing an Adapter is easy
+  - Determine the API you have and the API you need
+  - Create a component which aggregates (has a reference to,...) the adaptee
+  - Intermediate representations can pile up: use caching and other optimizations
  */
 
 // ==================Code==========================
